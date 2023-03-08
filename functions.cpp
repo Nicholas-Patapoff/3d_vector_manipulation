ffa4d339ba09447554fdacc57682ac5f3667318d
whitesila <nepatapoff@gmail.com>
2023-03-07 19:12:11 -0800

    completed cross and dot product functions!

--- functions.cpp
+++ functions.cpp
@@ -1,9 +1,42 @@
-class vector{
-    
-    
+#include<vector>
+#include<iostream>
+#include<cmath>
+class vector_var{
+    public:
+        double i[3] = {1.0, 0.0, 2.0};
+        double j[3] = {1.0, 3.0, 4.0};
+        double k[3] = {0,0,0};
+        double l[3] = {2.0, 4.0, 1.5};
+        vector_var(){}
+
+    double mag(double j[], double f[]);
+    double angle(double a[], double b[], double c[]);
+    void c_product(double a[], double b[], double* p);
 
 
 
-
+private:
 
 };
+
+
+double vector_var::mag(double a[], double b[]){
+        
+        return(sqrt(pow(a[0] - b[0], 2) + pow(a[1] - b[1], 2) + pow(a[2] - b[2], 2)));
+
+}
+
+
+double vector_var::angle(double a[], double b[], double c[]){
+
+    return(acos(a[0]/mag(a, c) * b[0]/mag(b, c) + a[1]/mag(a, c) * b[1]/mag(b, c) + a[2]/mag(a, c) * b[2]/mag(b, c)));  
+
+}
+
+void vector_var::c_product(double a[], double b[], double* p ){
+
+    p[0] = a[1] * b[2] - a[2] * b[1];
+    p[1] = a[2] * b[0] - a[0] * b[2];
+    p[2] = a[0] * b[1] - a[1] * b[0];
+
+}
--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
 #include<iostream> 
 #include<string>
-
+#include "functions.cpp"
 
 int main(){
 
-    
+
+
+vector_var trial;
+
+double angles = trial.angle(trial.i, trial.j, trial.k);
+
+
+std::cout << angles * 180 / acos(-1) << std::endl;
+
+double cross[3];
+
+trial.c_product(trial.i, trial.j, cross);
+
+for(int t = 0; t < 3; t++){
+
+    std::cout << cross[t] << " " ;
+
+}
+
     
 }
